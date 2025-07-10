87efc8c828f8a2cfb26982b19032aa9761bf9bc4
Ashmit tomar <ashmitt686@gmail.com>
2025-07-10 13:52:12 +0530

    Q13_RomanToInteger-Python

---
+++ Q13_RomanToInteger/main.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def romanToInt(self, s):
+        
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        total = 0
+        prev_value = 0
+
+        for char in reversed(s):
+            current_value = roman_map[char]
+            if current_value < prev_value:
+                total -= current_value
+            else:
+                total += current_value
+            prev_value = current_value
+
+        return total
+
+if __name__ == "__main__":
+    roman_string = input("Enter a Roman numeral: ").upper()
+    sol = Solution()
+    result = sol.romanToInt(roman_string)
+    print("Integer value:", result)
