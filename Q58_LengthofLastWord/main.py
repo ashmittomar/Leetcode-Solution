53df5b82c96c2a3be08bb850fbe83b6162a59364
Ashmit tomar <ashmitt686@gmail.com>
2025-07-19 21:39:40 +0530

    Q58_LengthofLastWord-Python

---
+++ Q58_LengthofLastWord/main.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        
+        words = s.strip().split()
+        return len(words[-1]) if words else 0
+
+user_input = input("Enter a sentence: ")
+sol = Solution()
+print("Length of last word:", sol.lengthOfLastWord(user_input))
