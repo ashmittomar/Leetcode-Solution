c60775b18d1e2c1a80b4a1cf224568375cf10786
Ashmit tomar <ashmitt686@gmail.com>
2025-08-27 21:36:20 +0530

    Q120_Triangle-Python

---
+++ Q120_Triangle/main.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        
+        # Bottom-up DP
+        for row in range(len(triangle) - 2, -1, -1):
+            for col in range(len(triangle[row])):
+                triangle[row][col] += min(triangle[row + 1][col], triangle[row + 1][col + 1])
+        return triangle[0][0]
+
+
+if __name__ == "__main__":
+    n = int(input("Enter number of rows in the triangle: "))
+    triangle = []
+    print("Enter the triangle values row by row:")
+    for i in range(n):
+        row = list(map(int, input().split()))
+        triangle.append(row)
+
+    sol = Solution()
+    result = sol.minimumTotal(triangle)
+    print("Minimum path sum:", result)
