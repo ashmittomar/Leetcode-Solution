bb759107d2ec93799792d34209d9a6359250a08d
Ashmit tomar <ashmitt686@gmail.com>
2025-09-08 21:46:44 +0530

    123_BestTimetoBuyandSellStockIII-Python

---
+++ Q123_BestTimetoBuyandSellStockIII/main.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        #start
+        if not prices:
+            return 0
+        
+        # Initialize states
+        buy1 = float('-inf')
+        sell1 = 0
+        buy2 = float('-inf')
+        sell2 = 0
+        
+        for price in prices:
+            buy1 = max(buy1, -price)         # First buy (spend money)
+            sell1 = max(sell1, buy1 + price) # First sell (gain profit)
+            buy2 = max(buy2, sell1 - price)  # Second buy (use profit from sell1)
+            sell2 = max(sell2, buy2 + price) # Second sell (final profit)
+        
+        return sell2
