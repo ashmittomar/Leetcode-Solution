060d1efd7de21a53c288b1a29f75b91dfe540220
Ashmit tomar <ashmitt686@gmail.com>
2025-09-09 20:59:58 +0530

    Q119_Pascal'sTriangleII-Python

--- Q119_Pascal'sTriangleII/main.py
+++ Q119_Pascal'sTriangleII/main.py
@@ -4,6 +4,6 @@ def getRow(rowIndex):
         for j in range(i - 1, 0, -1):  # update backwards
             row[j] += row[j - 1]
     return row
-#in
+
 rowIndex = int(input("Enter row index (0-indexed): "))
 print("Pascal's Triangle Row:", getRow(rowIndex))
