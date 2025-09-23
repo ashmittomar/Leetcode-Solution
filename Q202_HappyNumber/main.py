f2a2c9945dbca83e37767864f1b59d65b1db2e57
Ashmit tomar <ashmitt686@gmail.com>
2025-09-23 21:25:52 +0530

    .\Q202_HappyNumber\

---
+++ Q202_HappyNumber/main.py
@@ -0,0 +1,23 @@
+def get_next(number):
+    total = 0
+    while number > 0:
+        digit = number % 10
+        total += digit * digit
+        number //= 10
+    return total
+
+def is_happy(n):
+    seen = set()
+    while n != 1:
+        if n in seen:
+            return False
+        seen.add(n)
+        n = get_next(n)
+    return True
+
+# 
+n = int(input("Enter a number: "))
+if is_happy(n):
+    print(n, "is a Happy Number ✅")
+else:
+    print(n, "is NOT a Happy Number ❌")
