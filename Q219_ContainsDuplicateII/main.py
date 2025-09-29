9358e3d5bad2d4e869ab84cb58ab1aa366ee24f6
Ashmit tomar <ashmitt686@gmail.com>
2025-09-29 22:56:58 +0530

    Q219_ContainsDuplicateII-Python

--- Q219_ContainsDuplicateII/main.py
+++ Q219_ContainsDuplicateII/main.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
        
-        seen = {}  # stores number -> last index
+        seen = {}  
         for i, num in enumerate(nums):
             if num in seen and i - seen[num] <= k:
                 return True
