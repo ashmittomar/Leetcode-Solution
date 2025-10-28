30cd46f656d0ecf8f06bdedaf4aeaced411d37ac
Ashmit tomar <ashmitt686@gmail.com>
2025-10-28 21:01:59 +0530

    Q151_ReverseWordsinaString-Python

---
+++ Q151_ReverseWordsinaString/main.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def reverseWords(self, s):
+        words = s.split()
+        reversed_words = words[::-1]
+        return ' '.join(reversed_words)
+
+s = input()
+print(Solution().reverseWords(s))
