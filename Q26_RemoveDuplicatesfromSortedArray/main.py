087da4d688aa3c122ce899a860a622d512de992c
Ashmit tomar <ashmitt686@gmail.com>
2025-11-05 23:40:45 +0530

    Q26_RemoveDuplicatesfromSortedArray-Python

--- Q26_RemoveDuplicatesfromSortedArray/main.py
+++ Q26_RemoveDuplicatesfromSortedArray/main.py
@@ -1,18 +1,12 @@
 class Solution(object):
     def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+       
         if not nums:
             return 0
         
-        # Pointer for the position of unique elements
         k = 1
 
-        # Loop through the array starting from index 1
         for i in range(1, len(nums)):
-            # If current element is not equal to the previous unique one
             if nums[i] != nums[k - 1]:
                 nums[k] = nums[i]
                 k += 1
