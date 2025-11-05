384fc8965aa5130aa3f0ace6f34cea0b53ad4e35
Ashmit tomar <ashmitt686@gmail.com>
2025-11-05 23:39:24 +0530

    .\Q26_RemoveDuplicatesfromSortedArray\

---
+++ Q26_RemoveDuplicatesfromSortedArray/main.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        
+        # Pointer for the position of unique elements
+        k = 1
+
+        # Loop through the array starting from index 1
+        for i in range(1, len(nums)):
+            # If current element is not equal to the previous unique one
+            if nums[i] != nums[k - 1]:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
+
+
+# -------- User Input and Execution --------
+nums = list(map(int, input("Enter sorted numbers separated by space: ").split()))
+solution = Solution()
+k = solution.removeDuplicates(nums)
+
+print("Number of unique elements:", k)
+print("Array after removing duplicates:", nums[:k])
