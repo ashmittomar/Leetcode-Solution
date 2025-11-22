685ba29bf9c49cad5614f4890b60b21a7ba69204
Ashmit tomar <ashmitt686@gmail.com>
2025-11-22 23:39:08 +0530

    Q8_StringToInteger-Python

--- Q8_StringToInteger/main.py
+++ Q8_StringToInteger/main.py
@@ -28,6 +28,6 @@ class Solution(object):
 
 if __name__ == "__main__":
     user_input = input("Enter a string to convert to integer: ")
-    solution = Solion()
+    solution = Solution()
     result = solution.myAtoi(user_input)
     print("Converted integer:", result)
